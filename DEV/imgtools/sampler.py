d04c00abd4f753ff0fef71df013e6c2b8333e5d8
ricardoquinteladev <uc2020220508@student.uc.pt>
2023-02-23 17:46:24 +0000

    wip: adiciona semântica da subamostragem

--- DEV/imgtools/sampler.py
+++ DEV/imgtools/sampler.py
@@ -8,24 +8,24 @@ from cv2 import resize
 
 
 def down_sample(
-    y_channel: ndarray,
-    cb_channel: ndarray,
-    cr_channel: ndarray,
+    channel1: ndarray,
+    channel2: ndarray,
+    channel3: ndarray,
     scale: Tuple[int, int, int]
 ) -> Tuple[ndarray, ndarray, ndarray]:
     """Faz a subamostragem da imagem com a escala fornecida
 
     Args:
-        y_channel (ndarray): o canal Y
-        cb_channel (ndarray): o canal cb
-        cr_channel (ndarray): o canal cr
+        y_channel (ndarray): o primeiro canal
+        cb_channel (ndarray): o segundo canal
+        cr_channel (ndarray): o terceiro canal
         scale (Tuple[int, int, int]): a escala para a sub-amostragem {0,1,2,4}
 
     Returns:
         Tuple[ndarray, ndarray, ndarray]: os canais com a respetiva asubamostragem
     """
 
-    width, height = y_channel.shape
+    width, height = channel1.shape
 
     # 4 2 2
     # 4 2 0
@@ -40,10 +40,10 @@ def down_sample(
     if scale[2] == 0:
         width /= 2
 
-    cb_channel_resized = resize(cb_channel, (int(height), int(width)))
-    cr_channel_resized = resize(cr_channel, (int(height), int(width)))
+    channel2_resized = resize(channel2, (int(height), int(width)))
+    channel3_resized = resize(channel3, (int(height), int(width)))
 
-    return y_channel, cb_channel_resized, cr_channel_resized
+    return channel1, channel2_resized, channel3_resized
 
 
 def up_sample(
