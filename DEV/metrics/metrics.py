ff1a8238b8db45ad8d1712d9f25472abfb46fb3d
JoaoRafaelHenriques <joao.rafael.henriques@gmail.com>
2023-03-15 17:07:27 +0000

    feat: comentarios e 'pythonDoc' feitos para as funcoes das metricas

--- DEV/metrics/metrics.py
+++ DEV/metrics/metrics.py
@@ -1,4 +1,4 @@
-"""Calculo de metricas que verificam a qualidade do codec.
+"""Calcula as métricas relativas à compressão da imagem.
 """
 
 from numpy import sum as npsum, ndarray, float32, round as npround, max as npmax
@@ -11,7 +11,8 @@ from imgtools import add_padding
 from math import sqrt, log10
 
 def MSE(imagem_original: ndarray, imagem_reconstruida: ndarray) -> float:
-    """Calcula as métricas relativas à compressão da imagem
+    """Calcula a diferença média quadrada de entre os píxeis da imagem original
+    e da imagem reconstruída e apresenta as diferenças numa imagem preto e branco.
 
     Args:
         imagem_original (ndarray): a imagem original
@@ -39,16 +40,47 @@ def MSE(imagem_original: ndarray, imagem_reconstruida: ndarray) -> float:
     return npsum((imagem_original - imagem_reconstruida)**2) / (imagem_original.shape[0]*imagem_original.shape[1])
 
 
-def RMSE(MSE_value: float):
+def RMSE(MSE_value: float) -> float:
+    """Calcula a raíz da diferença média quadrada de entre os píxeis da imagem original
+    e da imagem reconstruída.
+
+    Args:
+        MSE_value (float): diferença média quadrada de entre os píxeis da imagem original
+    e da imagem reconstruída
+
+    Returns:
+        float: valor da raíz da diferença entre a imagem original e reconstruida
+    """
     return sqrt(MSE_value)
 
-def SNR(MSE_value: float, imagem_original: ndarray):
+def SNR(MSE_value: float, imagem_original: ndarray) -> float:
+    """Calcula a relação entre o original e o erro.
+
+    Args:
+        MSE_value (float): diferença média quadrada de entre os píxeis da imagem original
+    e da imagem reconstruída
+        imagem_original (ndarray): a imagem original
+
+    Returns:
+        float: relação entre o original e o erro
+    """
     
     power = npsum((imagem_original.astype(float32))**2) / (imagem_original.shape[0]*imagem_original.shape[1])
     
     return log10(power/MSE_value) * 10
 
-def PSNR(MSE_value: float, imagem_original: ndarray):
+def PSNR(MSE_value: float, imagem_original: ndarray) -> float:
+    """Calcula o rácio entre o quadrado do pico do sinal correcto (valor mais alto 
+    da imagem original) e o ruído (MSE).
+
+    Args:
+        MSE_value (float): diferença média quadrada de entre os píxeis da imagem original
+    e da imagem reconstruída
+        imagem_original (ndarray): a imagem original
+
+    Returns:
+        float: rácio entre o quadrado do pico do sinal correcto e o ruído
+    """
     
     max = npmax(imagem_original.astype(float32)) ** 2
     
